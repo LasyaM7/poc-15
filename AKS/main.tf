9733d6c53ecd5ec93764bdafe3fe72aacb8b1e6e
pavankattamuri8 <147482466+pavankattamuri8@users.noreply.github.com>
2026-06-16 12:03:08 +0530

    Update main.tf

--- AKS/main.tf
+++ AKS/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_D2s_v3"
+    vm_size    = "Standard_D2s_v7"
   }
 
   identity {
